a79e6e2c67f1efcdbb0278fd732948fc5099fc0b
cyoon1729 <chrisyoon1729@gmail.com>
2021-12-17 18:34:43 -0500

    Update packaging and imports in libs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,22 @@
 module Lib
-    ( someFunc
+    ( 
+      someFunc
+    , rabinKarpRoll
+    , rabinKarpMatch
+    , rabinKarp
+    , rabinKarpOnePass
     ) where
+
 import StringMatch.Hash
+import StringMatch.RabinKarp
+    (
+      rabinKarpRoll,
+      rabinKarpMatch,
+      rabinKarp,
+      rabinKarpOnePass
+    )
 
 someFunc :: IO ()
 someFunc = putStrLn $ unlines $ map show $ rollingHash "test" "--test-- test"
+
+
