e4200ca4afb63907effe54d25b54347c40afd4b8
Eumin Hong <milouhong45@gmail.com>
2021-12-18 19:03:03 -0500

    support for seqRabinKarp

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,10 @@ main :: IO ()
 main = do
     let pattern  = "TCTCCCAT"
         filePath = "data/human_g1k_v37.txt"
+    -- let pattern  = "question"
+    --     filePath = "data/hamlet.txt"
     -- text <- readFile filePath
     -- let matches = rabinKarp pattern text
-    matches <- parRabinKarp4 pattern filePath
+    -- matches <- parRabinKarp4 pattern filePath
+    matches <- seqRabinKarp pattern filePath
     print matches 
